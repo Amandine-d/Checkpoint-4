154c3c6a35de7e29376178a3e71a7a30ea2e31e7
Amandine <amandine.druart@gmail.com>
2022-02-02 10:25:36 +0100

    Server: Build connexion to DB and server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const { connection } = require("./db-connection");
+
+app.use(express.json());
+// app.use("/api", mainRouter);
+
+const server = app.listen(process.env.PORT || 8000, (err) => {
+  // eslint-disable-next-line no-console
+  if (err) return console.log(err.message);
+  // eslint-disable-next-line no-console
+  console.log(`Connexion succeded at PORT: http://localhost:${process.env.PORT || 8000}`);
+  return connection.connect((err) => {
+    // eslint-disable-next-line no-console
+    if (err) return console.log(err.message);
+    // eslint-disable-next-line no-console
+    return console.log(`DB connexion ok`);
+  });
+});
+
+module.exports = server;
